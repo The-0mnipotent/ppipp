3576748ab48b584662600b6f782a52786d283750
Arpit Singh <arpit84_soe@jnu.ac.in>
2024-04-04 17:19:56 +0530

    HomePage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,13 @@
+import React from "react";
+import "./Header.css";
+
 function Header() {
   return (
-    <div>
-      {/* top */}
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Discover your next <span className="text-slate-500">perfect</span>
-          <br />
-          home with ease
-        </h1>
-        <div className="text-gray-500 text-xs font-bold sm:text-sm">
-          Unlock Your Dream Home with PropertyPulse - Your Ultimate Destination
-          for Finding the Perfect Place to Live! <br /> Explore Our Vast
-          Selection of Properties Today!
-        </div>
-      </div>
+    <div className="header">
+      <h1 className="heading">PPiPP</h1>
+      <h2 className="subheading">
+        Prediction of Protein-protein interacting position pairs
+      </h2>
     </div>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,114 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Input from "../components/Input";
+import Navigation from "../components/Navigation";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="header">
-        <h1 className="heading">PPiPP</h1>
-        <h2 className="subheading">
-          Prediction of Protein-protein interacting position pairs
-        </h2>
-      </div>
+      <Header />
       <div className="content">
-        <div className="navigation">
-          <div className="navigation-list">
-            <h3 className="list-heading">Menu</h3>
-            <ul>
-              <li>
-                <Link to="/" className="nav-link">
-                  Introduction
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="nav-link">
-                  Sample Output
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="nav-link">
-                  Expected server Performance (for pair or single chain
-                  prediction; benchmarked over all dimers)
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="nav-link">
-                  Expected server Performance (for single residue prediction;
-                  benchmarked over multimers; contacts with any chain)
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="nav-link">
-                  Datasets (PDB IDs), used in training
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="navigation-list">
-            <h3 className="list-heading">Citation</h3>
-            <ul>
-              <li>
-                <Link to="/" className="nav-link">
-                  Partner-Aware Prediction of Interacting Residues in
-                  Protein-Protein Complexes from Sequence Data Shandar Ahmad *
-                  and Kenji Mizuguchi
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="navigation-list">
-            <h3 className="list-heading">Contact</h3>
-            <ul>
-              <li>
-                <Link to="/" className="nav-link">
-                  Shandar Ahmad
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="nav-link">
-                  Arpit Singh
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="input-section">
-          <p>
-            <span className="intro">
-              This web server takes fasta sequences of two proteins and predicts
-              pairs of interacting residues between them.
-              <br /> The input is a pair of amino acid sequences in the fasta
-              format.{" "}
-            </span>
-            <br />
-            <span className="warning">
-              Sequence size for each protein is limited to 300 residues. Longer
-              sequences will be truncated to this length.
-            </span>
-          </p>
-          <div className="input">
-            <textarea placeholder="Enter text"></textarea>
-            <textarea placeholder="Enter text"></textarea>
-          </div>
-          <button className="submit-button">Submit</button>
-          <p>
-            <div className="title">Test Sequence</div>
-            <div className="description">
-              Copy and paste full or part of the following sequences into the
-              text boxes above and press the predict button to test this web
-              server.
-            </div>
-            <div className="sample">
-              DVSGTVCLSALPPEATDTLNLIASDGPFPYSQDGVV
-              FQNRESVLPTQSYGYYHEYTVITPGARTRGTRRIIT GEATQEDYYTGDHYATFSLIDQTC
-            </div>
-            <div className="sample">
-              KKAVINGEQIRSISDLHQTLKKELALPEYYGENLDA
-              LWDCLTGWVEYPLVLEWRQFEQSKQLTENGAESVL VFREAKAEGCDITIILS
-            </div>
-          </p>
-        </div>
+        <Navigation />
+        <Input />
       </div>
     </div>
   );
